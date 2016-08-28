3c7a4a57a9ee21f3f51ad6c6c8ca8e9331b13d19
Hoang Nam <namhoang.td09@gmail.com>
2016-08-28 19:49:47 +0700

    add node server

---
+++ server.js
@@ -0,0 +1,39 @@
+var http = require('http');
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+var hostname, port = 3000;
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      // console.log(iface.address);
+    	hostname = iface.address;
+    }
+    ++alias;
+  });
+});
+
+// var port = 3000;
+
+var server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
