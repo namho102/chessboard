8824b9e26b67daf1fd0028e7620a082d85551cd1
Hoang Nam <namhoang.td09@gmail.com>
2016-08-29 19:41:06 +0700

    read serial data

--- serial_test.js
+++ serial_test.js
@@ -1,4 +1,15 @@
 var SerialPort = require("serialport");
-var port = new SerialPort("/dev/tty-usbserial1", {
-  baudRate: 57600
+var port = new SerialPort("/dev/ttyACM0", {
+  baudRate: 9600,
+  parser: SerialPort.parsers.readline('\n')
+});
+
+// port.open(function (err) {
+//   if (err) {
+//     return console.log('Error opening port: ', err.message);
+//   }
+// });
+
+port.on('data', function (data) {
+  console.log(data.toString());
 });
